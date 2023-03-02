d3e0a6b6e8b74e508dd80670dd246e1209c4a380
zoi-ui <124429965+zoi-ui@users.noreply.github.com>
2023-03-02 09:39:22 +0600

    Update Animal.java

--- problem-A/Animal.java
+++ problem-A/Animal.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.stream.Stream;
 
-public class pra2 {
+public class Animal {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Animal animal = new Animal("Animal");
